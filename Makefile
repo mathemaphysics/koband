968f820b2a9298e3cfc1a403b23f8b6742bd13ee
Ryan P. Daly <mathemaphysics@gmail.com>
2019-01-04 01:23:33 -0600

    Started the template script in perl
    Also added experimental CMakeLists.txt along
    with the Makefile.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+# Makefile
+
+packmol.inp: config.$(CONFIG_EXT)
+	# Make packmol.inp from parameters
 
 # vim: tw=65:ts=4:sts=4:sw=4:et:sta
--- configure.pl
+++ configure.pl
@@ -1,5 +1,17 @@
 #!/usr/bin/perl
 
-use Template
+use strict;
+use warnings;
+use Template;
+
+my $file = 'src/greeting.html';
+my $vars = {
+    message  => "Hello World\n"
+};
+
+my $template = Template->new();
+
+$template->process($file, $vars)
+|| die "Template process failed: ", $template->error(), "\n";
 
 # vim: tw=65:ts=4:sts=4:sw=4:et:sta

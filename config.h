384c85588517c1178f9825570309122f21e690bc
Ryan P. Daly <mathemaphysics@gmail.com>
2019-01-08 00:02:42 -0600

    Final form for input files

--- config.h
+++ config.h
@@ -4,7 +4,6 @@
 #define PROJECT "koband"
 #define LOGFILE "${LOGGING_ROOT}/${PROJECT}.log"
 #define TR 0.55
-#define TA
 #define NA 4000
 #define NB 1000
 #define DX 54.727
--- configure.pl
+++ configure.pl
@@ -3,15 +3,46 @@
 use strict;
 use warnings;
 use Template;
+use Modern::Perl;
+use Env;
+use Env qw(LOGGING_ROOT);
 
-my $file = 'src/greeting.html';
+# Put the input variables here
+my $project = "koband";
+my $logging_root = $LOGGING_ROOT;
+
+# Can do calculations inline now
+my @ifiles = ('config.txt.tt', 'packmol.inp.tt');
 my $vars = {
-    message  => "Hello World\n"
+    project  => "$project",
+    logfile  => "$logging_root/$project",
+    ma       => 0.039948,
+    mb       => 0.039948,
+    sigaa    => 0.340100,
+    epsaa    => 0.978638,
+    sigab    => '',
+    epsab    => '',
+    sigbb    => '',
+    epsbb    => '',
+    tr       => 0.55,
+    na       => 4000,
+    nb       => 1000,
+    dx       => 54.727,
+    pdba     => 'kaA.pdb',
+    pdbb     => 'kaB.pdb',
 };
 
 my $template = Template->new();
 
-$template->process($file, $vars)
-|| die "Template process failed: ", $template->error(), "\n";
+foreach ( @ifiles ) {
+    print $_, "\n";
+    my $ifile = $_;
+    s/.tt$//;
+    my $ofile = $_;
+    say "$ofile";
+    $template->process($ifile, $vars, $ofile)
+        || die "Template process failed: ", $template->error(), "\n";
+}
+
 
 # vim: tw=65:ts=4:sts=4:sw=4:et:sta

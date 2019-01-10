d95936dccaf0833b2fd892d0e53724affcbcd05a
Ryan P. Daly <mathemaphysics@gmail.com>
2019-01-10 04:00:07 -0600

    Slight changes to a few files on guile/

--- guile/guileext02-gnumake.c
+++ guile/guileext02-gnumake.c
@@ -7,19 +7,20 @@
 
 int plugin_is_GPL_compatible;
 
-char *
-hello (const char *nm, unsigned int argc, char **argv)
+char * hello (const char *nm, unsigned int argc, char **argv)
 {
-  int len = strlen (argv[0]) + 7;
-  char *buf = gmk_alloc (len);
-  sprintf (buf, "echo Hello %s", argv[0]);
-  printf("hello!\n");
-  return buf;
+	int len = strlen (argv[0]) + 7;
+	char *buf = gmk_alloc (len);
+	sprintf (buf, "echo Hello %s", argv[0]);
+	printf("hello!\n");
+    gmk_free(buf);
+	return NULL;
 }
 
-int
-guileext02_gmk_setup ()
+int guileext02_gmk_setup ()
 {
-  gmk_add_function ("hello", hello, 1, 1, 1);
-  return 1;
+	gmk_add_function ("hello", hello, 1, 1, 1);
+	return 1;
 }
+
+// vim: tw=65:ts=4:sts=4:sw=4:et:sta
